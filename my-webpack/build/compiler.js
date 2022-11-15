48a1989cb7ef381cf446e6f16d16f4f3dff86065
houxinchao <houxinchao@jd.com>
2022-11-15 18:19:59 +0800

    webpack 生命周期模拟使用tapable

--- my-webpack/build/compiler.js
+++ my-webpack/build/compiler.js
@@ -1,9 +1,10 @@
+const {AsyncSeriesHook} = 'tapable'
 /**
  * webpack 是基于tapable事件流，tapable本质是发布订阅，可以使webpack在特定的时间节点触发相应的hook
  * 在webpack整个生命周期中传递的一直是compiler对象，
  */
 class Compiler {
-  constructor(options) {
+  constructor(context, options) {
     this.options = options
     this.hooks = Object.freeze({
       // {AsyncSeriesHook}是tapable框架提供的事件触发函数：表示异步串行
@@ -22,7 +23,7 @@ class Compiler {
     // ast
     // parse
     // traverse
-    // 分析的过程中就可以使用loader
+    // 分析的过程中就可以使用loader进行解析
   }
   // 发送
   emit() {
@@ -34,9 +35,37 @@ class Compiler {
   // 执行
   run() {
     console.log('执行自定义webpack')
-    this.buildMoudle()
+    // run的时候开始分析构建模块
+    // this.buildMoudle()
+    this.hooks.beforeRun.callAsync(this, err=>{
+      if(err) return
+      this.hooks.run.callAsync(this, err=>{
+        if(err) return
+        this.complie(this.buildMoudle)
+      })
+    })
+  }
+  complie(callback) {
+    const params = ''
+    this.hooks.beforeCompile.callAsync(params, err => {
+      this.hooks.compile.call(params);
+      this.hooks.make.callAsync('', err=> {
+        
+      })
+    })
   }
 }
 export function createCompiler(options) {
-  return new Compiler(options)
+  const compiler = new Compiler(options.context, options)
+  // 开发插件使用插件调用方法apply方法
+  if(Array.isArray(options.plugins)) {
+    options.plugins.forEach(plugin => {
+      if(typeof plugin === 'function') {
+        plugin.call(compiler, compiler)
+      } else {
+        plugin.apply(compiler)
+      }
+    });
+  }
+  return compiler
 }
--- my-webpack/build/loader.js
+++ my-webpack/build/loader.js
@@ -1,4 +1,5 @@
 module.exports = (source) =>{
   console.log(source)
+  // 从后向前处理
   return 'hello'
 }
